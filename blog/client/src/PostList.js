a9d334b9d858ae15c3571b8e8bb5fecb735b8f70
Simon Kragh <sika@netcompany.com>
2022-10-12 23:17:33 +0200

    added commentlist

--- blog/client/src/PostList.js
+++ blog/client/src/PostList.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import CommentCreate from './CommentCreate';
+import CommentList from './CommentList';
+
 
 const PostList = () => {
 
@@ -25,6 +27,7 @@ const PostList = () => {
                 >
                     <div className="card-body">
                         <h3>{post.title}</h3>
+                        <CommentList postId = {post.id} />
                         <CommentCreate postId = {post.id}/>
                     </div>
         </div>
