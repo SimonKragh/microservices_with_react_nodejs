1101a885239f8857441c65dd246e2518d1e99bed
Simon Kragh <sika@netcompany.com>
2022-10-13 20:41:41 +0200

    Introduced Query Service

--- blog/client/src/PostList.js
+++ blog/client/src/PostList.js
@@ -9,8 +9,9 @@ const PostList = () => {
     const [posts, setPosts] = useState({});
 
     const fetchPosts = async () => {
-        const res = await axios.get('http://localhost:4000/posts');
+        const res = await axios.get('http://localhost:4002/posts');
         setPosts(res.data);
+        console.log(res.data);
     };
 
     useEffect(() => {
@@ -27,7 +28,7 @@ const PostList = () => {
                 >
                     <div className="card-body">
                         <h3>{post.title}</h3>
-                        <CommentList postId = {post.id} />
+                        <CommentList comments = {post.comments} />
                         <CommentCreate postId = {post.id}/>
                     </div>
         </div>
