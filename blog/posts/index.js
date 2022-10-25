be1b75b24bcb44548b088ead90bdfa68e73874be
SimonKragh <simonkraghandersen@gmail.com>
2022-10-25 21:48:21 +0200

    k8s deployment config

--- blog/posts/index.js
+++ blog/posts/index.js
@@ -39,5 +39,6 @@ app.post('/events', (req, res) => {
 });
 
 app.listen(4000, () => {
-    console.log("Posts listening on 4000.")
+    console.log("New version 60");
+    console.log("Posts listening on 4000.");
 });
