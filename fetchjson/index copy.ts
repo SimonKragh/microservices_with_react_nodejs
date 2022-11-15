14089b1c71dd5c1bc5f883d0e3b6bf8573ff4b33
SimonKragh <simonkraghandersen@gmail.com>
2022-11-15 20:39:15 +0100

    Added routes for auth

---
+++ fetchjson/index copy.ts
@@ -0,0 +1,40 @@
+import axios from 'axios';
+
+
+const url = 'https://jsonplaceholder.typicode.com/todos/1'
+
+interface Todo {
+    id: number;
+    title: string;
+    completed: boolean;
+}
+
+axios.get(url).then(response => {
+    const todo = response.data as Todo;
+    const id = todo.id;
+    const title = todo.title;
+    const completed = todo.completed;
+    logTodo(id, title, completed);
+
+});
+
+const logTodo = (id: number, title: string, completed: boolean) => {
+    console.log(`
+    The Todo with ID: ${id}
+    Has a title of: ${title}
+    Is it finished?: ${completed}
+    `);
+};
+
+
+const fetchData = async () => {
+    console.log("And using fetchdata!")
+    const todo = (await axios.get(url)).data;
+    console.log(todo)
+    const id = todo.id;
+    const title = todo.title;
+    const completed = todo.completed;
+    logTodo(id, title, completed);
+};
+
+fetchData(); 
