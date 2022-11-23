10f29d86de90d1e93733d8e9d31a6a4f505eef6a
SimonKragh <simonkraghandersen@gmail.com>
2022-11-23 10:42:26 +0100

    Finished erorr handling

--- ticketing/auth/src/index.ts
+++ ticketing/auth/src/index.ts
@@ -1,9 +1,13 @@
-import express from "express";
-import { json } from "body-parser";
-import { currentUserRouter } from "./routes/current-user";
-import { signinRouter } from "./routes/signin";
-import { signoutRouter } from "./routes/signout";
-import { signupRouter } from "./routes/signup";
+import express from 'express';
+import 'express-async-errors';
+import { json } from 'body-parser';
+import { currentUserRouter } from './routes/current-user';
+import { signinRouter } from './routes/signin';
+import { signoutRouter } from './routes/signout';
+import { signupRouter } from './routes/signup';
+import { errorHandler } from './middlewares/error-handler';
+import { NotFoundError } from './errors/not-found-error';
+import { getEffectiveTypeParameterDeclarations } from 'typescript';
 
 const app = express();
 app.use(json());
@@ -13,7 +17,12 @@ app.use(signinRouter);
 app.use(signoutRouter);
 app.use(signupRouter);
 
-app.listen(3000, () => {
-    console.log("Listening on port 3000")
+app.all('*', async (req, res) => {
+  throw new NotFoundError();
 });
 
+app.use(errorHandler);
+
+app.listen(3000, () => {
+  console.log('Listening on port 3000');
+});
