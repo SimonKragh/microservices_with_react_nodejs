f5461cc975caa94d30d645d0d48f198b3fad1029
SimonKragh <simonkraghandersen@gmail.com>
2022-12-05 22:12:33 +0100

    FInished Section 9 (Auth)

--- ticketing/auth/src/routes/current-user.ts
+++ ticketing/auth/src/routes/current-user.ts
@@ -1,21 +1,11 @@
 import express from 'express';
-import jwt from 'jsonwebtoken';
+import { currentUser } from '../middlewares/current-user';
 
 const router = express.Router();
 
-router.get('/api/users/currentuser', (req, res) => {
+router.get('/api/users/currentuser', currentUser, (req, res) => {
   console.log(`[Current User] Received Request:`, req.session?.jwt);
-  if (!req.session?.jwt) {
-    return res.send({ currentUser: null });
-  }
-
-  try {
-    const payload = jwt.verify(req.session?.jwt, process.env.JWT_KEY!);
-    return res.send({ currentUser: payload });
-  } catch (err) {
-    console.log(`[Current User] Error:`, err);
-    return res.send({ currentUser: null });
-  }
+  res.send({ currentUser: req.currentUser || null });
 });
 
 export { router as currentUserRouter };
