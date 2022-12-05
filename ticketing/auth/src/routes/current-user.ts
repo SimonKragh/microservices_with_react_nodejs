b188c86bec53a76b41636bbca7d3b8abb159fbbb
SimonKragh <simonkraghandersen@gmail.com>
2022-12-05 21:35:27 +0100

    Added currentUser and signout

--- ticketing/auth/src/routes/current-user.ts
+++ ticketing/auth/src/routes/current-user.ts
@@ -1,9 +1,21 @@
 import express from 'express';
+import jwt from 'jsonwebtoken';
 
 const router = express.Router();
 
 router.get('/api/users/currentuser', (req, res) => {
-    res.send('Hi there!')
+  console.log(`[Current User] Received Request:`, req.session?.jwt);
+  if (!req.session?.jwt) {
+    return res.send({ currentUser: null });
+  }
+
+  try {
+    const payload = jwt.verify(req.session?.jwt, process.env.JWT_KEY!);
+    return res.send({ currentUser: payload });
+  } catch (err) {
+    console.log(`[Current User] Error:`, err);
+    return res.send({ currentUser: null });
+  }
 });
 
-export { router as currentUserRouter }
+export { router as currentUserRouter };
