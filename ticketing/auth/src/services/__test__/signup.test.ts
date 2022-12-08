cd7024c5ab60014f849672c2949adf928b2130e0
SimonKragh <simonkraghandersen@gmail.com>
2022-12-08 11:49:01 +0100

    Added tests for signup api

--- ticketing/auth/src/services/__test__/signup.test.ts
+++ ticketing/auth/src/services/__test__/signup.test.ts
@@ -1,3 +1,4 @@
+import { response } from 'express';
 import request from 'supertest';
 import { app } from '../../app';
 
@@ -11,7 +12,7 @@ it('returns a 201 on successful signup', async () => {
     .expect(201);
 });
 
-it('returns a 400 on invalid email during signup', async () => {
+it('returns a status of 400 on invalid email during signup', async () => {
   return request(app)
     .post('/api/users/signup')
     .send({
@@ -19,4 +20,60 @@ it('returns a 400 on invalid email during signup', async () => {
       password: 'validPassword',
     })
     .expect(400);
+});
+
+it('returns a status of 400 on password that does not match criterias (length) during signup', async () => {
+  return request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'valid@email.com',
+      password: '123',
+    })
+    .expect(400);
+});
+
+it('returns a status of 400 if email or password is empty', async () => {
+  await request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'valid@email.com',
+    })
+    .expect(400);
+
+  await request(app)
+    .post('/api/users/signup')
+    .send({
+      password: 'validPassword',
+    })
+    .expect(400);
+});
+
+it('Disallows duplicate emails. e.g. the email is our primary business key.', async () => {
+  await request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'valid@email.com',
+      password: 'validPassword',
+    })
+    .expect(201);
+
+  await request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'valid@email.com',
+      password: 'validPassword',
+    })
+    .expect(400);
+});
+
+it('sets a cookie on the client after a successful signup', async () => {
+  const response = await request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'valid@email.com',
+      password: 'validPassword',
+    })
+    .expect(201);
+
+  expect(response.get('Set-Cookie')).toBeDefined();
 });
